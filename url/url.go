175707ca3a2ab25a5eafd826a5c2ce1d6250d773
aulianafahrian <aulianafahrian@gmail.com>
2023-03-30 10:22:20 +0700

    komit

--- url/url.go
+++ url/url.go
@@ -10,7 +10,7 @@ import (
 func Web(page *fiber.App) {
 	page.Post("/api/whatsauth/request", controller.PostWhatsAuthRequest)  //API from user whatsapp message from iteung gowa
 	page.Get("/ws/whatsauth/qr", websocket.New(controller.WsWhatsAuthQR)) //websocket whatsauth
-	page.Get("/", controller.Homepage)                                    //ujicoba panggil package musik
+	page.Get("/", controller.Home)                                        //ujicoba panggil package musik
 	page.Get("/presensi", controller.GetPresensi)
 	page.Get("/proyek1", controller.GetProyek1)
 	page.Get("/mahasiswa", controller.GetDataMahasiswa)
