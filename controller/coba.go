9fe38b53d9b8c08d545da2aaa119c5bb368788b8
aulianafahrian <aulianafahrian@gmail.com>
2023-05-31 14:46:47 +0700

    up

--- controller/coba.go
+++ controller/coba.go
@@ -18,6 +18,15 @@ import (
 	inimodullatihan "github.com/indrariksa/be_presensi/module"
 )
 
+// GetAllPresensi godoc
+// @Summary Get All Data Presensi.
+// @Description Mengambil semua data presensi.
+// @Tags Presensi
+// @Accept json
+// @Produce json
+// @Success 200 {object} Presensi
+// @Router /presensi [get]
+
 func Home(c *fiber.Ctx) error {
 	return c.Status(fiber.StatusOK).JSON(fiber.Map{
 		"github_repo": "https://github.com/aulianafahrian/aufa-ulbi",
